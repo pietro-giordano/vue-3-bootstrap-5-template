a2dfdb0cf16ff5941acd25501efe57e6bdbf041a
Pietro Giordano <giopi87@gmail.com>
2024-04-26 17:16:03 +0200

    fix

--- src/pages/Homepage.vue
+++ src/pages/Homepage.vue
@@ -7,7 +7,7 @@ export default {
 <template>
     <div class="container mt-3">
         <h1>Vue3 + Bootstrap 5.3</h1>
-        <p>Template basato su Vue3 e Bootstrap 5.3. Presente anche vue-router, sass, fontawesome-icons.</p>
+        <p>Template basato su Vue3 e Bootstrap 5.3. Presente anche vue-router, axios, sass, fontawesome-icons.</p>
     </div>
 </template>
 
